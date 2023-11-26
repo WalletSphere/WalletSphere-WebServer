16d92ac2bdbabef36baed3a1476d8b80df24dcf3
Max <maksimhomisak64@gmail.com>
2023-11-26 22:10:48 +0100

    updated API

--- src/main/java/com/khomishchak/ws/controllers/ExchangerController.java
+++ src/main/java/com/khomishchak/ws/controllers/ExchangerController.java
@@ -8,15 +8,8 @@ import com.khomishchak.ws.model.response.FirstlyGeneratedBalanceResp;
 import com.khomishchak.ws.model.response.SyncDataResp;
 import com.khomishchak.ws.services.exchangers.ExchangerService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -45,7 +38,7 @@ public class ExchangerController {
         return exchangerService.getAllMainBalances(userId);
     }
 
-    @DeleteMapping("/{balanceId}/balance")
+    @DeleteMapping("/balances/{balanceId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAccountBalance(@PathVariable long balanceId) {
         exchangerService.deleteExchangerForUser(balanceId);
@@ -61,7 +54,7 @@ public class ExchangerController {
         return exchangerService.synchronizeBalanceDataForUser(userId);
     }
 
-    @PostMapping("/synchronize/deposit-withdrawal-history")
+    @PostMapping("/deposit-withdrawal-history/synchronize")
     public List<ExchangerDepositWithdrawalTransactions> synchronizeDWTransactionsHistory(@RequestAttribute Long userId) {
         return exchangerService.synchronizeDepositWithdrawalTransactionsData(userId);
     }
