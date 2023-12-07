fce48debb32e8538376e36952f18712a80040713
Maksym Khomishchak <mkhomishchak@amelco.co.uk>
2023-12-07 13:05:02 +0000

    remove unused classes

--- src/main/java/com/khomishchak/ws/exceptions/exceptionRules/ExceptionRule.java
+++
@@ -1,8 +0,0 @@
-package com.khomishchak.ws.exceptions.exceptionRules;
-
-import org.springframework.http.HttpStatus;
-
-public interface ExceptionRule {
-    Class<? extends RuntimeException> getType();
-    HttpStatus getHttpStatus();
-}
